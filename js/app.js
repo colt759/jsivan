4ad925e29c639ab31f3f156eff646442c276c0e9
colt759 <colt8800@outlook.com>
2020-11-02 22:09:23 -0600

    lesson 12.практика первого приложения

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 "use strict";
+// lesson12
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели", "");
+const a = prompt("Один из посмледних фильмов?", "");
+const b = +prompt("Насколько оцените его?", "");
+const c = prompt("Один из посмледних фильмов?", "");
+const d = +prompt("Насколько оцените его?", "");
+
+
+
+const personalMuvieDb = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMuvieDb.movies[a] = b;
+personalMuvieDb.movies[c] = d;
+
+console.log(personalMuvieDb);
