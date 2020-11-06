f32f088ec2e6fdc3039572b6496acfa3e0feba55
colt759 <colt8800@outlook.com>
2020-11-06 07:39:11 -0600

    lesson 34 События на мобильных устройствах

--- lessons/js/app.js
+++ lessons/js/app.js
@@ -1,77 +1,34 @@
 "use strict";
-// lesson18 Практика, часть 3
 
+//Урок 34 События на мобильных устройствах
 
+//touchstart
+//touchmove
+//touched
+//touchenter
+//touchleave
+//touchcancel
 
+window.addEventListener('DOMContentLoaded', () => {
+  const box = document.querySelector('.box');
+  box.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    console.log('start');
+    console.log(e.targetTouches[0].pageX);
 
-// let numberOfFilms;
+  });
 
-// function start() {
-//   numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
-//   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//     numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
-//   }
-// }
-// // start();
-// const personalMuvieDb = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
+   box.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    console.log('move');
+   });
+  
+   box.addEventListener("touchend", (e) => {
+     e.preventDefault();
+     console.log("end");
+   });
+});
 
-// function writeYourGenres() {
-//   for (let i = 1; i < 4; i++) {
-//     const film = prompt(`Ваш любимый фильм под номером ${i} ?`, "");
-//     if (film != null && film != "") {
-//       personalMuvieDb.genres[i-1] = film;
-//     } else {
-//       i--;
-//     }
-//   }
-// }
-
-// writeYourGenres();
-// console.log(personalMuvieDb);
-
-// function rememberMyFilms() {
-//   for (let i = 0; i < 2; i++) {
-//     const a = prompt("Один из посмледних фильмов?", "");
-//     const b = +prompt("Насколько оцените его?", "");
-
-//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//       personalMuvieDb.movies[a] = b;
-//     } else {
-//       i--;
-//     }
-//   }
-// }
-
-// // rememberMyFilms();
-
-// function detectPersonallevel() {
-//   if (personalMuvieDb.count <= 10) {
-//     alert("Просмотрено довольно мало фильмов");
-//   } else if (personalMuvieDb.count > 10 && personalMuvieDb.count <= 30) {
-//     alert("Вы класический зритель");
-//   } else if (personalMuvieDb.count > 30) {
-//     alert("Вы киноман");
-//   } else {
-//     console.log("Произошла ошибка");
-//   }
-// }
-
-// // detectPersonallevel();
-
-// function showMyDb(hidden) {
-//   if (!hidden) {
-//     console.log(personalMuvieDb);
-//   }
-// }
-
-// showMyDb(personalMuvieDb.privat);
-
-///--------------------------------------------------
-
-
+// touches список всех пальцев на экране
+// targetTouches все пальцы которые взаимодействуют с конкретным елементом
+// chengedTouches список пальцев участвующих в текущем событии
