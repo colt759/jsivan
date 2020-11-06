635883fe3f64d0f9c567af14e0b342312487c8ff
colt759 <colt8800@outlook.com>
2020-11-06 07:58:11 -0600

    lesson 35  Async, defer, динамические скрипты

--- lessons/js/app.js
+++ lessons/js/app.js
@@ -1,34 +1,16 @@
 "use strict";
 
-//Урок 34 События на мобильных устройствах
+// const script = document.createElement('script');
+// script.src = "js/test.js";
+// script.async = false;
+// document.body.append(script);
 
-//touchstart
-//touchmove
-//touched
-//touchenter
-//touchleave
-//touchcancel
+function loadScript(src) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.async = false;
+  document.body.append(script);
+}
+loadScript("test.js");
+loadScript("apps.js");
 
-window.addEventListener('DOMContentLoaded', () => {
-  const box = document.querySelector('.box');
-  box.addEventListener('touchstart', (e) => {
-    e.preventDefault();
-    console.log('start');
-    console.log(e.targetTouches[0].pageX);
-
-  });
-
-   box.addEventListener('touchmove', (e) => {
-    e.preventDefault();
-    console.log('move');
-   });
-  
-   box.addEventListener("touchend", (e) => {
-     e.preventDefault();
-     console.log("end");
-   });
-});
-
-// touches список всех пальцев на экране
-// targetTouches все пальцы которые взаимодействуют с конкретным елементом
-// chengedTouches список пальцев участвующих в текущем событии
